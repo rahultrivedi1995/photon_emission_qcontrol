Fixing gradient issue.

cmps_utils.py
@@ -10,37 +10,30 @@ from typing import Optional
 
 
 def compute_single_ph_proj(state: tn.onedim.MatrixProductState) -> np.ndarray:
-    """Compute the single-photon component of the matrix product state.
-
-    Args:
-        state: The state to be considered expressed as a matrix product state
-            object.
-
-    Returns:
-        The single-photon state as a numpy array.
-    """
     # We first extract the tensors corresponding to the zero and single photons
     # in the time-bins.
     tensors_0 = [state.data[n].data[:, :, 0] for n in range(state.nsites)]
     tensors_1 = [state.data[n].data[:, :, 1] for n in range(state.nsites)]
     # Multiply with all the vacuum tensors except for the first one.
-    tensor_prod_right = tensors_0[1]
-    for t in tensors_0[2:]:
-        tensor_prod_right = tensor_prod_right @ t
-    # We will also maintain products from the left.
-    tensor_prod_left = tensors_0[0]
+    tensor_prod_forward = [tensors_0[0]]
+    tensor_prod_backward = [tensors_0[-1]]
+    for k in range(state.nsites - 1):
+        tensor_prod_forward.append(
+                tensor_prod_forward[-1] @ tensors_0[k + 1])
+        tensor_prod_backward.append(
+                tensors_0[state.nsites - k - 2] @ tensor_prod_backward[-1])
 
     # Calculate the single photon projection.
-    single_ph_proj = [np.trace(tensors_1[0] @ tensor_prod_right)]
-    for tstep in range(1, state.nsites - 1):
-       tensor_prod_right = np.linalg.inv(tensors_0[tstep]) @ tensor_prod_right
+    single_ph_proj = [np.trace(tensors_1[0] @ tensor_prod_backward[-2])]
+    for t in range(1, state.nsites - 1):
        single_ph_proj.append(
-           np.trace(tensor_prod_left @ tensors_1[tstep] @ tensor_prod_right))
-       tensor_prod_left = tensor_prod_left @ tensors_0[tstep]
+           np.trace(tensor_prod_forward[t - 1] @
+                    tensors_1[t] @
+                    tensor_prod_backward[-2 - t]))
+    single_ph_proj.append(
+            np.trace(tensor_prod_forward[-2] @ tensors_1[-1]))
 
-    single_ph_proj.append(np.trace(tensor_prod_left @ tensors_1[-1]))
     return np.flip(single_ph_proj)
-
 
 def compute_photon_number_exp(
         state: tn.onedim.MatrixProductState) -> np.ndarray:

oqs_emission_mps.py
@@ -17,7 +17,7 @@ def _get_transfer_matrix_list(
     transfer_matrices = []
     for site in range(left_mps.nsites):
         left_tensor = left_mps.data[site].data
-        right_tensor = left_mps.data[site].data
+        right_tensor = right_mps.data[site].data
         tensor_prod = [np.kron(left_tensor[:, :, i].conj(),
                                right_tensor[:, :, i])
                        for i in range(left_tensor.shape[-1])]
@@ -203,6 +203,20 @@ class OqsSystem(metaclass=abc.ABCMeta):
         """Compute the inner product of the output photons with given MPS."""
         mps_state = self.get_mps(pulses)
         return tn.onedim.inner_product_mps(target_mps, mps_state)
+
+    def get_inner_prod_tmat(
+            self,
+            target_mps: tn.onedim.MatrixProductState,
+            pulses: List[pulse.Pulse]) -> complex:
+        # This function is just to check if the transfer matrix implementation
+        # of the MPS is consistent. It is more efficient to use the
+        # `get_inner_prod_tmat` function for actual computation.
+        mps_state = self.get_mps(pulses)
+        transfer_matrices = _get_transfer_matrix_list(target_mps, mps_state)
+        tmat = transfer_matrices[0]
+        for t in transfer_matrices[1:]:
+            tmat = tmat @ t
+        return tmat[0, 0]
 
     def _get_deriv_mps(
             self,

parameterized_pulse.py
@@ -46,8 +46,13 @@ class DirectParameterizedPulse(ParameterizedPulse):
         self._dt = max_time / num_tsteps
         self._bounds = bounds
         if init_state is None:
-            init_state = np.random.uniform(bounds[0], bounds[1], num_tsteps)
+            init_state = np.random.uniform(
+                    bounds[0], bounds[1], num_tsteps + 1)
         self._state = init_state
+
+    def bounds(self) -> List[Tuple[float, float]]:
+        return np.array([(self._bounds[0], self._bounds[1])
+                         for _ in range(self.state().size)], dtype=float)
 
     def state(self) -> np.ndarray:
         """Returns the state of the pulse."""
@@ -64,10 +69,10 @@ class DirectParameterizedPulse(ParameterizedPulse):
         # the pulse length. Outside the pulse length, the pulse is assumed to
         # be 0, so the gradient vanishes.
         time_inds_mod = [t for b, t in zip(bins, time_inds)
-                         if b != 0 or b != self._num_tsteps + 1]
-        bins_mod = [b for b in bins if b != 0 or b != self._num_tsteps + 1]
+                         if b != 0 and b != self._num_tsteps + 1]
+        bins_mod = [b for b in bins if b != 0 and b != self._num_tsteps + 1]
         # Fill in the gradients.
-        grad = np.zeros((self._num_tsteps, times.size), dtype=complex)
+        grad = np.zeros((self._num_tsteps + 1, times.size), dtype=complex)
         grad[np.array(bins_mod), np.array(time_inds_mod)] = 1.0
         return grad
 